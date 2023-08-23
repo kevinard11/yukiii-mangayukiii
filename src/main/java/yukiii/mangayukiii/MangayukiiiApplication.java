ecf04973255c9c749c4e1896d0a331a143883ce0
kevinard11 <drlobsterard@gmail.com>
2023-08-23 18:55:09 +0700

    Initial commit

---
+++ src/main/java/yukiii/mangayukiii/MangayukiiiApplication.java
@@ -0,0 +1,13 @@
+package yukiii.mangayukiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangayukiiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangayukiiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yukiii/mangayukiii/MangayukiiiApplicationTests.java
@@ -0,0 +1,13 @@
+package yukiii.mangayukiii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangayukiiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
