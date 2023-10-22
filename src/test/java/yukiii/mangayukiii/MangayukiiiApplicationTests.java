81b8d8b3e0da03ee74a7fdbf1081c02203cf2deb
kevin.ardela <kevin.ardela@sinarmasmsiglife.co.id>
2023-10-22 16:42:18 +0700

    update unit test

--- src/test/java/yukiii/mangayukiii/MangayukiiiApplicationTests.java
+++ src/test/java/yukiii/mangayukiii/MangayukiiiApplicationTests.java
@@ -1,13 +1,8 @@
 package yukiii.mangayukiii;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MangayukiiiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
